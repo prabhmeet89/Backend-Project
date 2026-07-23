4c61f4d4ee39aaea4151eb81a1106eca0c29b4f6
Meet <meet97094@gmail.com>
2026-07-23 13:16:59 +0530

    Add user account management routes: change password, fetch current user, update account details, and manage user avatars and cover images

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { userRegister, loginUser , logoutUser, refreshAcessToken } from "../controllers/user.controllers.js";
+import { userRegister, loginUser , logoutUser, refreshAcessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, GetUserProfileChannel, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,7 +25,11 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-Token").post(refreshAcessToken)
-
-
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)  // verify jwt check kar rha hai ki user logged in hai ki ni middleware 
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-Account detail").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar") ,updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage") ,updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,GetUserProfileChannel) // params wale mai (url) mai humne oehle se he username bata rkha hai to usme routes iis tarike se likhte hai
+router.route("/History").get(verifyJWT,getWatchHistory)
 export default router
